221a405dfd489a589a919804bc907531dbb1d047
Anna Gavin <L00188483@atu.ie>
2025-04-26 15:15:55 +0100

    changed SGs to inline rules

--- security_groups.tf
+++ security_groups.tf
@@ -1,3 +1,4 @@
+
 resource "aws_security_group" "alb_sg" {
   name   = "alb_sg"
   vpc_id = aws_vpc.main.id
@@ -5,95 +6,84 @@ resource "aws_security_group" "alb_sg" {
   tags = {
     Name = "alb-sg"
   }
-}
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "alb_sg_allow_http" {
-  security_group_id = aws_security_group.alb_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_egress_rule" "alb_egress_all" {
-  security_group_id = aws_security_group.alb_sg.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-
-resource "aws_security_group" "public_sg" {
-  name        = "public_sg"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "public_sg"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-
 
 
 
 resource "aws_security_group" "private_sg" {
-  name        = "private_sg"
-  vpc_id      = aws_vpc.main.id
+  name   = "private_sg"
+  vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "private_sg"
   }
-}
 
-resource "aws_vpc_security_group_ingress_rule" "private_sg_allow_http_from_alb" {
-  security_group_id = aws_security_group.private_sg.id
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-  referenced_security_group_id = aws_security_group.alb_sg.id
-}
+  ingress {
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    security_groups             = [aws_security_group.alb_sg.id]
+  }
 
+  # not necessary, but useful for debugging
+  ingress {
+    from_port                   = 80
+    to_port                     = 80
+    protocol                    = "tcp"
+    security_groups             = [aws_security_group.public_sg.id]
+    description                 = "Allow HTTP from Jumpbox"
+  }
 
-# not necessary, useful for debugging
-resource "aws_vpc_security_group_ingress_rule" "private_sg_allow_http_from_jumpbox" {
-  security_group_id = aws_security_group.private_sg.id
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
-  referenced_security_group_id = aws_security_group.public_sg.id
-}
+  ingress {
+    from_port                   = 22
+    to_port                     = 22
+    protocol                    = "tcp"
+    security_groups             = [aws_security_group.public_sg.id]
+    description                 = "Allow SSH from Jumpbox"
+  }
 
-resource "aws_vpc_security_group_ingress_rule" "private_sg_allow_ssh" {
-  security_group_id = aws_security_group.private_sg.id
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-  referenced_security_group_id = aws_security_group.public_sg.id
-}
-
-resource "aws_vpc_security_group_egress_rule" "private_egress_all" {
-  security_group_id = aws_security_group.private_sg.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
-}
-
-
-
-###############################
-# SGs for Jumpox
-##############################
-
-resource "aws_vpc_security_group_ingress_rule" "public_sg_allow_ssh" {
-  security_group_id = aws_security_group.public_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
-resource "aws_vpc_security_group_egress_rule" "public_egress_all" {
-  security_group_id = aws_security_group.public_sg.id
-  ip_protocol       = "-1"
-  cidr_ipv4         = "0.0.0.0/0"
+resource "aws_security_group" "public_sg" {
+  name   = "public_sg"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public_sg"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
